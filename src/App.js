5a1cb8bb2595cbdbf5b6d7d30d2dab4c6c6f2811
Tuturtut <arthursimonin88@gmail.com>
2023-09-11 21:41:47 +0200

    test

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis esse inventore harum omnis vitae architecto voluptatum quidem voluptates, cumque dolores suscipit pariatur animi ab molestias veritatis ut nulla autem exercitationem.
-      </p>
+      <p>Patate</p>
     </div>
   );
 }
