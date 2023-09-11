be3f545046dcd3447793cba11a81cbdd9b361869
Tuturtut <arthursimonin88@gmail.com>
2023-09-11 17:38:29 +0200

    retest

--- src/App.js
+++ src/App.js
@@ -4,22 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>GROS TEST</h1>
-        <h2>Moeyn test</h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis esse inventore harum omnis vitae architecto voluptatum quidem voluptates, cumque dolores suscipit pariatur animi ab molestias veritatis ut nulla autem exercitationem.
+      </p>
     </div>
   );
 }
