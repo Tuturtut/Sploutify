062a54a41838fe6b5c586768f6fd1f0c0a33d247
Tuturtut <arthursimonin88@gmail.com>
2023-09-11 17:27:48 +0200

    petit test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>GROS TEST</h1>
+        <h2>Petit test</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
